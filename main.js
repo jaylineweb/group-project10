030b93e22109582a4b0554b33ef7ed4b4e96e83f
Jeonghoon Lee <darkjay118@gmail.com>
2024-07-16 20:52:36 +0900

    mobile용 버튼 추가 및 style.css 수정

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 // 모든 song-item 요소들을 선택
 let songItems = document.querySelectorAll('.song-item');
 
@@ -12,4 +14,22 @@ songItems.forEach(item => {
         // 마우스를 뗐을 때 active 클래스 제거
         item.classList.remove('active');
     });
+});
+
+
+//닫기 버튼용
+let header = document.getElementById('header');
+let closeBtn = document.querySelector('.close-btn');
+
+const navBarActivate=()=>{
+    header.classList.add('on');
+}
+
+closeBtn.addEventListener('click',()=>{
+    console.log('닫기')
+    header.classList.remove('on');
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 760) closeMenu(); // mobile(760px) 변형 시(resizing) 사이드배너가 열린 상태로 view되는걸 방지
 });
